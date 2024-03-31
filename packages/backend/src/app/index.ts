3cfbebf9aa884574dd2f186488c8eae042c4c58d
pumbas600 <joshjeffers600@gmail.com>
2024-03-31 21:08:45 +1300

    refactor: move BigInt.toJSON function to a utils file

--- packages/backend/src/app/index.ts
+++ packages/backend/src/app/index.ts
@@ -2,13 +2,7 @@ import { Server } from "node:http";
 import express, { Express } from "express";
 import { prisma } from "../client";
 import config from "../config";
-
-// TODO: Move this to a more appropriate location
-// Make BigInt JSON serializable. See: https://github.com/GoogleChromeLabs/jsbi/issues/30
-// @ts-ignore This causes an error when running the server because toJSON doesn't exist. (But that's okay because we're adding it here!)
-BigInt.prototype.toJSON = function (): string {
-  return this.toString();
-};
+import "../utils/bigInt"; // Make BigInt JSON serializable
 
 export interface ExpressServer {
   app: Express;
