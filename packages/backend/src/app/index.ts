cd26911e5738cbb21ab23072cad5dead4c51b42a
pumbas600 <joshjeffers600@gmail.com>
2024-04-01 13:28:14 +1300

    feat: add pngjs dependency

--- packages/backend/src/app/index.ts
+++ packages/backend/src/app/index.ts
@@ -2,8 +2,8 @@ import { Server } from "node:http";
 import express, { Express } from "express";
 import { prisma } from "../client";
 import config from "../config";
-import "../utils"; // Make BigInt JSON serializable
 import { apiRouter } from "../routes";
+import "../utils"; // Make BigInt JSON serializable
 
 export interface ExpressServer {
   app: Express;
--- packages/backend/src/routes/api/v1/canvas.ts
+++ packages/backend/src/routes/api/v1/canvas.ts
@@ -5,3 +5,7 @@ export const canvasRouter = Router();
 canvasRouter.get("/", async (req, res) => {
   res.status(200).json({ message: "Hello, World!" });
 });
+
+canvasRouter.get("/:canvasId", async (req, res) => {
+  // TODO: Validate that the canvasId is a number
+});
