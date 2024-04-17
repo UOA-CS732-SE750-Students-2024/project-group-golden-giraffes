359740580f062d877db361b144136ee6846fa79c
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-04-17 13:57:55 +1200

    fix build error

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "../styles/core.css";
 import { Theme } from "./theme";
 
