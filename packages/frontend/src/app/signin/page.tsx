1092a35f75348dbb03c7e7b3c29a50cbb74e7287
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-04-17 13:54:48 +1200

    organise imports

--- packages/frontend/src/app/signin/page.tsx
+++ packages/frontend/src/app/signin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Image from "next/image";
 import { Button, Typography, styled } from "@mui/material";
+import Image from "next/image";
 
 const Background = styled("div")`
   align-items: center;
