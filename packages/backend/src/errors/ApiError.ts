d7dcef6749eb99d6efb75c197c4132f14f0022bf
pumbas600 <joshjeffers600@gmail.com>
2024-04-23 23:50:19 +1200

    docs: add jsdocs to the applyToResponse method

--- packages/backend/src/errors/ApiError.ts
+++ packages/backend/src/errors/ApiError.ts
@@ -8,6 +8,12 @@ export default class ApiError extends Error {
     super(message);
   }
 
+  /**
+   * This method provides a default way of applying an error to a response. Subclasses can override
+   * this method to provide more complex error responses.
+   *
+   * @param res The response to apply the error to
+   */
   public applyToResponse(res: Response): void {
     res.status(this.status).json({ message: this.message });
   }
