f33d2d7742d7dfa1391d4bbce96374a961930eb3
pumbas600 <joshjeffers600@gmail.com>
2024-04-26 20:44:30 +1200

    feat: make prisma query to get event palette

--- packages/backend/src/routes/api/v1/palette.ts
+++ packages/backend/src/routes/api/v1/palette.ts
@@ -1,7 +1,11 @@
+import { getPaletteForEvent } from "@/services/paletteService";
 import { Router } from "express";
 
 export const paletteRouter = Router();
 
-paletteRouter.get("/", async (req, res) => {});
+paletteRouter.get("/", async (req, res) => {
+  getPaletteForEvent(2023);
+  res.end();
+});
 
 paletteRouter.get("/:eventId", async (req, res) => {});
--- packages/backend/src/services/paletteService.ts
+++ packages/backend/src/services/paletteService.ts
@@ -1,5 +1,35 @@
 import { prisma } from "@/client";
+import { PaletteDto } from "@blurple-canvas-web/types";
 
-export async function getPaletteForEvent(eventId: number): Promise<void> {
-  const test = prisma.color.findUniqueOrThrow();
+export async function getPaletteForEvent(eventId: number): Promise<PaletteDto> {
+  const eventPalette = await prisma.color.findMany({
+    select: {
+      id: true,
+      code: true,
+      global: true,
+      name: true,
+      rgba: true,
+      participations: {
+        select: {
+          guild: { select: { invite: true } }, // Include the guild invite
+        },
+        // Only include the participation for the event we're looking at
+        where: {
+          event_id: eventId,
+        },
+      },
+    },
+    // Filter the colours to only include global colours or colours that are part of the event
+    where: {
+      OR: [
+        { global: true },
+        {
+          participations: { some: { event_id: eventId } },
+        },
+      ],
+    },
+  });
+
+  // TODO: Map the eventPalette to a PaletteDto
+  return {} as PaletteDto;
 }
