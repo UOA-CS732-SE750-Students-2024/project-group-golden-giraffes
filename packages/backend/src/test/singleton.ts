135c1356785288b709bce50dd9c29fe46db66ca7
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-04-28 00:14:02 +1200

    fix: import redorder for prisma so the mocking actually works

--- packages/backend/src/test/singleton.ts
+++ packages/backend/src/test/singleton.ts
@@ -1,10 +1,11 @@
-import { prisma } from "@/client";
 import { PrismaClient } from "@prisma/client";
 import { DeepMockProxy, mockDeep, mockReset } from "jest-mock-extended";
 
+import { prisma } from "@/client";
+
 jest.mock("@/client", () => ({
   __esModule: true,
-  default: mockDeep<PrismaClient>(),
+  prisma: mockDeep<PrismaClient>(),
 }));
 
 beforeEach(() => {
