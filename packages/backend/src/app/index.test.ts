c8a79789765a45fe15242427138994a9e1a7dcff
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-04-28 22:58:15 +1200

    test: add example for prisma calling in tests

--- packages/backend/src/app/index.test.ts
+++ packages/backend/src/app/index.test.ts
@@ -1,6 +1,9 @@
 import request from "supertest";
 import { createApp } from "./index";
 
+import { prisma } from "@/client";
+import { PrismockClient } from "prismock";
+
 // Instantiates the entire server of the app here since the hello world route is not exported
 // It would be better to create an express app at the start and then add the specific route
 const { app, server } = createApp();
@@ -17,6 +20,34 @@ describe("Hello world test", () => {
       await request(app).get("/").expect(200, { message: "Hello, world!" });
     });
   });
+});
+
+describe("Test Prisma Stuff", () => {
+  const testCanvas = {
+    name: "Test Canvas",
+    locked: true,
+    width: 100,
+    height: 100,
+    start_coordinates: [1, 1],
+  };
+
+  it("can find no canvases", async () => {
+    const canvas = await prisma.canvas.findMany();
+    expect(canvas).toStrictEqual([]);
+  });
+
+  it("can write to in memory prisma instance", async () => {
+    prisma.canvas.create({ data: testCanvas });
+    const canvas = await prisma.canvas.findFirst({ where: { id: 1 } });
+    expect(canvas?.name).toStrictEqual("Test Canvas");
+  });
+
+  it("resets after each test", async () => {
+    prisma.canvas.create({ data: testCanvas });
+    const canvas = await prisma.canvas.findMany();
+    expect(canvas.length).toEqual(1);
+  });
+  //
 });
 
 server.close();
