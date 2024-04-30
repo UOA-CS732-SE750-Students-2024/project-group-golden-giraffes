6811691ca3936a42fcac6fc6c2edb195ecf4cd9a
PolarWolf314 <aaronjoshguo@gmail.com>
2024-04-30 14:22:42 +1200

    refactor: remove eslint ignore because we aren't using eslint

--- packages/backend/src/types/index.d.ts
+++ packages/backend/src/types/index.d.ts
@@ -1,7 +1,6 @@
 import { DiscordUserLoginInfo } from "@blurple-canvas-web/types";
 declare global {
   namespace Express {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface User extends DiscordUserLoginInfo {}
   }
 }
