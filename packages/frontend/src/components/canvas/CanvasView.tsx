73e0f8cd20a6807381d2a606f18e6295ed762fcb
Josh Jeffers <38820160+pumbas600@users.noreply.github.com>
2024-05-01 22:52:28 +1200

    feat: panning (#60)
    * feat: add simple canvas component for creating canvas from image url
    
    * chore: move theme out of app directory
    
    * feat: add loading state to canvas component
    
    * config: define api url in config file
    
    * feat: create animated text component...
    
    not sure if we want to keep this though...
    
    * feat: make canvas take up full page and support children being overlaid
    
    * feat: add a placeholder card over canvas and style default card  to match figma designs
    
    * style: make canvas take up entire screen and get it to display crisply
    
    * docs: add some comments explaining the canvas useEffect logic
    
    * feat: testing panning/zooming
    
    * feat: get panning working using transform css
    
    * feat: add function for getting viewport dimensions
    
    * feat: support panning on desktop and clamp pan offset
    
    * style: fix card container covering top of canvas
    
    * feat: add hook for listen to the screen dimensions
    
    * feat: use screen size to clamp canvas position after resize
    
    * refactor: derive is loading from image being null
    
    * refactor: move points and helper methods to separate file
    
    * refactor: extract center canvas logic into its own function
    
    * chore: use @/ import paths for theme and styles
    
    * feat: simplify panning and remove the need for centering it
    
    * feat: add scale and default it to filling the screen
    
    * refactor: change listeners from mouse to pointer
    
    * chore: revert change from pointer back to mouse
    
    to properly handle touch moment, I'll use the touch events
    
    * feat: scale mouse diff by zoom scale
    
    * feat: display the full canvas initially with some small padding on large devices
    
    * docs: add jsdocs to clarify functionality
    
    * chore: change fullscreen container to use main element

--- packages/frontend/src/components/canvas/CanvasView.tsx
+++ packages/frontend/src/components/canvas/CanvasView.tsx
@@ -1,28 +1,18 @@
 "use client";
 
+import { getScreenDimensions } from "@/hooks/useScreenDimensions";
 import { CircularProgress, styled } from "@mui/material";
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
+import { ORIGIN, Point, addPoints, diffPoints, scalePoint } from "./point";
 
-export interface CanvasViewProps {
-  imageUrl: string;
-  children?: ReactNode;
-}
+const FullscreenContainer = styled("main")`
+  position: fixed;
 
-const FullscreenContainer = styled("div")`
-  height: 100vh;
-  width: 100vw;
-  overflow: hidden;
-
-  & canvas {
-    image-rendering: pixelated;
-    position: fixed;
-    z-index: -1;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+  > * {
+    position: relative;
   }
 
-  & .loader {
+  .loader {
     position: fixed;
     top: 50%;
     left: 50%;
@@ -30,27 +20,78 @@ const FullscreenContainer = styled("div")`
   }
 `;
 
+const CanvasContainer = styled("div")`
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  canvas {
+    image-rendering: pixelated;
+    max-width: inherit;
+  }
+`;
+
+/**
+ * Return the value clamped so that it is within the range [min, max].
+ */
+function clamp(value: number, min: number, max: number): number {
+  return Math.min(Math.max(value, min), max);
+}
+
+/**
+ * Calculate the default scale to use for the canvas. This is the required scaling to get the canvas
+ * to cover the entire screen.
+ */
+function getDefaultScale(image: HTMLImageElement): number {
+  const screenDimensions = getScreenDimensions();
+
+  // Don't add any padding on the initial scale for small devices
+  const padding = screenDimensions.width < 500 ? 0 : 50;
+
+  const scale = Math.min(
+    (screenDimensions.width - padding) / image.width,
+    (screenDimensions.height - padding) / image.height,
+  );
+
+  return scale;
+}
+
+export interface CanvasViewProps {
+  imageUrl: string;
+  children?: ReactNode;
+}
+
 export default function CanvasView({ imageUrl, children }: CanvasViewProps) {
-  const [isLoading, setIsLoading] = useState(true);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const lastMousePosRef = useRef(ORIGIN);
+
+  const [image, setImage] = useState<HTMLImageElement | null>(null);
+  const [scale, setScale] = useState(1);
+  const [offset, setOffset] = useState(ORIGIN);
+
+  const isLoading = image === null;
 
   useEffect(() => {
-    setIsLoading(true);
-
     const image = new Image();
     image.onload = () => {
       if (!canvasRef.current) return;
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-      if (!ctx) return;
 
-      setIsLoading(false);
+      const context = canvasRef.current.getContext("2d");
+      if (!context) return;
 
       // We need to set the width of the canvas first, otherwise if the image is bigger than
       // the canvas it'll get cut off.
-      canvas.width = image.width;
-      canvas.height = image.height;
-      ctx.drawImage(image, 0, 0);
+      canvasRef.current.width = image.width;
+      canvasRef.current.height = image.height;
+
+      context.drawImage(image, 0, 0);
+
+      setImage(image);
+      setScale(getDefaultScale(image));
     };
     image.src = imageUrl;
 
@@ -60,9 +101,82 @@ export default function CanvasView({ imageUrl, children }: CanvasViewProps) {
     };
   }, [imageUrl]);
 
+  /********************************
+   * PANNING FUNCTIONALITY.       *
+   ********************************/
+
+  /**
+   * Ensure that the offset is within bounds. This is defined as at least half the canvas being on
+   * screen along an axis.
+   */
+  const clampOffset = useCallback(
+    (offset: Point): Point => {
+      if (image == null) return offset;
+
+      const widthLimit = image.width / 2;
+      const heightLimit = image.height / 2;
+
+      return {
+        x: clamp(offset.x, -widthLimit, widthLimit),
+        y: clamp(offset.y, -heightLimit, heightLimit),
+      };
+    },
+    [image],
+  );
+
+  const handleMouseMove = useCallback(
+    (event: MouseEvent): void => {
+      const lastMousePos = lastMousePosRef.current;
+      const currentMousePos: Point = { x: event.pageX, y: event.pageY }; // use document so can pan off element
+      lastMousePosRef.current = currentMousePos;
+
+      const mouseDiff = scalePoint(
+        diffPoints(currentMousePos, lastMousePos),
+        scale,
+      );
+
+      setOffset((prevOffset) => {
+        const newOffset = addPoints(prevOffset, mouseDiff);
+        return clampOffset(newOffset);
+      });
+    },
+    [scale, clampOffset],
+  );
+
+  /**
+   * Remove the mouse move listener when the mouse is released to stop panning.
+   */
+  const handleMouseUp = useCallback((): void => {
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+  }, [handleMouseMove]);
+
+  /**
+   * Only add the mouse move listener when you click down so that moving your mouse normally doesn't
+   * cause the canvas to pan.
+   */
+  const handleStartPan = useCallback(
+    (event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+      lastMousePosRef.current = { x: event.pageX, y: event.pageY };
+    },
+    [handleMouseMove, handleMouseUp],
+  );
+
   return (
     <FullscreenContainer>
-      <canvas ref={canvasRef} id="canvas" />
+      <CanvasContainer onMouseDown={handleStartPan}>
+        <div
+          id="canvas-pan-and-zoom"
+          style={{
+            transform: `translate(${offset.x}px, ${offset.y}px)`,
+            scale,
+          }}
+        >
+          <canvas ref={canvasRef} />
+        </div>
+      </CanvasContainer>
       {isLoading && <CircularProgress className="loader" />}
       {children}
     </FullscreenContainer>
