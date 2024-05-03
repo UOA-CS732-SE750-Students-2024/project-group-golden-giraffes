f3140cdf09e83bac60b855dfbfbaff7896264e43
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-05-03 23:07:50 +1200

    test: add canvasId into add initializeCanvases

--- packages/backend/src/test/initializeCanvases.ts
+++ packages/backend/src/test/initializeCanvases.ts
@@ -12,12 +12,19 @@ export default function intializeCanvases() {
   prisma.canvas.create({
     data: {
       ...testCanvas,
+      id: 1,
       name: "Unlocked Canvas",
       locked: false,
       event_id: 1,
     },
   });
   prisma.canvas.create({
-    data: { ...testCanvas, name: "Locked Canvas", locked: true, event_id: 9 },
+    data: {
+      ...testCanvas,
+      id: 9,
+      name: "Locked Canvas",
+      locked: true,
+      event_id: 9,
+    },
   });
 }
