8adf843b3c1eb28f70b181dac08212ec87899e07
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-05-04 14:47:30 +1200

    less ugly placeholder sidebar

--- packages/frontend/src/components/action-panel/ActionPanel.tsx
+++ packages/frontend/src/components/action-panel/ActionPanel.tsx
@@ -2,12 +2,14 @@
 
 import { styled } from "@mui/material";
 
-const ColorfulBox = styled("div")`
-  background-color: red;
-  width: 100%;
+const Container = styled("div")`
+  border: oklch(100% 0 0 / 15%) solid 3px;
+  background-color: var(--color-not-quite-black);
+  border-radius: 1.5rem;
   height: 100%;
+  width: 100%;
 `;
 
 export default function ActionPanel() {
-  return <ColorfulBox />;
+  return <Container />;
 }
