70752ba87587016af501987beaff4aa510a87c8c
Samuel Ou <sou323@aucklanduni.ac.nz>
2024-05-04 16:51:48 +1200

    refactor: removed redundant return

--- packages/backend/src/routes/api/v1/statistics.ts
+++ packages/backend/src/routes/api/v1/statistics.ts
@@ -18,7 +18,7 @@ statisticsRouter.get("/user/:userId/:canvasId", async (req, res) => {
     const userId = req.params.userId;
     const canvasId = await parseCanvasId({ canvasId: req.params.canvasId });
     const stats = await getUserStats(userId, canvasId);
-    return res.status(200).json(stats);
+    res.status(200).json(stats);
   } catch (error) {
     ApiError.sendError(res, error);
   }
@@ -28,7 +28,7 @@ statisticsRouter.get("/leaderboard/:canvasId", async (req, res) => {
   try {
     const canvasId = await parseCanvasId(req.params);
     const leaderboard = await getLeaderboard(canvasId);
-    return res.status(200).json(leaderboard);
+    res.status(200).json(leaderboard);
   } catch (error) {
     ApiError.sendError(res, error);
   }
