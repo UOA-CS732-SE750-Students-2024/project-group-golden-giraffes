dd4d27ddc815a5c538d5b729a48a2688de3d12e0
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-05-04 15:41:29 +1200

    wip: navbar links

--- packages/frontend/src/components/nav/navbar.tsx
+++ packages/frontend/src/components/nav/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { styled } from "@mui/material";
+import Link from "next/link";
 
 const Nav = styled("nav")`
   border-block-end: oklch(100% 0 0 / 15%) solid 3px;
@@ -9,37 +10,39 @@ const Nav = styled("nav")`
   gap: 1rem;
   grid-template-columns: auto 1fr;
   justify-content: flex-end;
-  padding-block: 1rem;
-  padding-inline: 3.5rem;
+  padding: 1rem 3.5rem;
   place-items: center flex-end;
 `;
 
 const CompositeLogo = styled("div")`
+  align-items: center;
   display: flex;
   gap: 1.5rem;
-  align-items: center;
+  user-select: none;
 `;
 
 const Wordmark = styled("div")`
-  font-weight: 900;
+  color: var(--discord-white);
   font-variation-settings: "wdth" 125;
+  font-weight: 900;
+  text-decoration: none;
 `;
-
-const SignOutLink = styled("a")``;
 
 export default function Navbar() {
   return (
     <Nav>
-      <CompositeLogo>
-        <img
-          alt="Blurple Canvas logo"
-          src="/images/blurple-canvas-logo~dark.svg"
-          width={28}
-          height={28}
-        />
-        <Wordmark>Blurple Canvas</Wordmark>
-      </CompositeLogo>
-      <SignOutLink>Sign out</SignOutLink>
+      <Link href="/">
+        <CompositeLogo>
+          <img
+            alt="Blurple Canvas logo"
+            src="/images/blurple-canvas-logo~dark.svg"
+            width={28}
+            height={28}
+          />
+          <Wordmark>Blurple Canvas</Wordmark>
+        </CompositeLogo>
+      </Link>
+      <a href="/">Sign out</a>
     </Nav>
   );
 }
