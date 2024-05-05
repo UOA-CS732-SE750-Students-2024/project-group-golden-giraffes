103660c13baf9990174255d95fb1bf47a125af0a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-05-05 20:49:04 +1200

    port env

--- packages/frontend/src/config/index.ts
+++ packages/frontend/src/config/index.ts
@@ -1,6 +1,8 @@
 const config = {
   apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
-  baseUrl: process.env.BASE_URL || "http://localhost:3000",
+  baseUrl:
+    process.env.BASE_URL || `http://localhost:${process.env.PORT || 3000}`,
+  port: process.env.PORT || 3000,
 } as const;
 
 export default config;
