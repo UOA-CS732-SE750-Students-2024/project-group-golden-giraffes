371edba6d214043f9b15a3f5f387522c53e8920a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-05-05 16:01:44 +1200

    delete unwanted types

--- packages/types/src/Participation.d.ts
+++
@@ -1,9 +0,0 @@
-import { Guild } from "./Guild";
-import { PaletteColor } from "./paletteColor";
-
-export interface Participation {
-  guildId: number;
-  guild: Guild;
-  event: BlurpleEvent;
-  color?: PaletteColor;
-}
