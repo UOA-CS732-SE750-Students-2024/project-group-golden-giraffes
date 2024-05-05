a183abbd8864f925274c1d0f7908c1c8092609b8
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-05-05 20:47:26 +1200

    configure metadataBase

--- packages/frontend/src/config/index.ts
+++ packages/frontend/src/config/index.ts
@@ -1,5 +1,6 @@
 const config = {
   apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+  baseUrl: process.env.BASE_URL || "http://localhost:3000",
 } as const;
 
 export default config;
