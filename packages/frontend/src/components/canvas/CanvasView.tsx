5409775cb4a19c9a8c4c18a44d01b2f083647328
Josh Jeffers <38820160+pumbas600@users.noreply.github.com>
2024-05-05 15:10:16 +1200

    feat: mobile panning (#62)
    * refactor: use event.movementX to figure out movement diff
    
    * feat: support touch panning
    
    * refactor: move the common update offset logic into its own method
    
    * refactor: only add event listeners to canvas container
    
    * fix: use svh for canvas container to prevent scrolling on mobile
    
    * fix: panning causing the page to refresh on mobile
    
    * fix: overscroll behavior not applying to some mobile browsers
    
    * fix: prevent pull to refresh on all mobile browsers using JS
    
    * fix: layout and cancel mouse event when moving out of container
    
    * fix: get default scale using container size rather than screen size
    
    * chore: remove duplicate css property
    
    * chore: remove js for preventing pull to refresh :pensive:

--- packages/frontend/src/components/canvas/CanvasView.tsx
+++ packages/frontend/src/components/canvas/CanvasView.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { CircularProgress, styled } from "@mui/material";
-import { useCallback, useEffect, useRef, useState } from "react";
+import {
+  ReactNode,
+  Touch,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { ORIGIN, Point, addPoints, scalePoint } from "./point";
 
 import { Dimensions, getScreenDimensions } from "@/hooks/useScreenDimensions";
 import { clamp } from "@/util";
 import { CanvasPicker } from ".";
-import { ORIGIN, Point, addPoints, diffPoints, scalePoint } from "./point";
 
 const CanvasContainer = styled("div")`
   background-color: var(--discord-old-not-quite-black);
@@ -27,6 +34,10 @@ const CanvasContainer = styled("div")`
     user-select: none;
   }
 
+  .loader {
+    position: fixed;
+  }
+
   canvas {
     image-rendering: pixelated;
     max-width: inherit;
@@ -34,27 +45,33 @@ const CanvasContainer = styled("div")`
 `;
 
 /**
- * Calculate the default scale to use for the canvas. This is the required scaling to get the canvas
- * to cover the entire screen.
+ * Calculate the default scale to use for the canvas. This tries to maximise the size of the canvas
+ * without it overflowing the screen.
  */
-function getDefaultScale(image: HTMLImageElement): number {
-  const screenDimensions = getScreenDimensions();
-
+function getDefaultScale(
+  container: HTMLDivElement,
+  image: HTMLImageElement,
+): number {
   // Don't add any padding on the initial scale for small devices
-  const padding = screenDimensions.width < 500 ? 0 : 50;
+  const padding = container.clientWidth < 500 ? 0 : 50;
 
   const scale = Math.min(
-    (screenDimensions.width - padding) / image.width,
-    (screenDimensions.height - padding) / image.height,
+    (container.clientWidth - padding) / image.width,
+    (container.clientHeight - padding) / image.height,
   );
 
   return scale;
 }
 
-export default function CanvasView({ imageUrl }: { imageUrl: string }) {
+export interface CanvasViewProps {
+  imageUrl: string;
+}
+
+export default function CanvasView({ imageUrl }: CanvasViewProps) {
   const imageRef = useRef<HTMLImageElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const lastMousePosRef = useRef(ORIGIN);
+  const startTouchesRef = useRef<Touch[]>([]);
 
   const [imageDimensions, setImageDimension] = useState<Dimensions | null>(
     null,
@@ -77,7 +94,12 @@ export default function CanvasView({ imageUrl }: { imageUrl: string }) {
 
     context.drawImage(image, 0, 0);
 
-    setScale(getDefaultScale(image));
+    if (containerRef.current) {
+      setScale(getDefaultScale(containerRef.current, image));
+    } else {
+      setScale(1);
+    }
+
     setImageDimension({ width: image.width, height: image.height });
   }, []);
 
@@ -113,51 +135,114 @@ export default function CanvasView({ imageUrl }: { imageUrl: string }) {
     [imageDimensions],
   );
 
-  const handleMouseMove = useCallback(
-    (event: MouseEvent): void => {
-      const lastMousePos = lastMousePosRef.current;
-      const currentMousePos: Point = { x: event.pageX, y: event.pageY }; // use document so can pan off element
-      lastMousePosRef.current = currentMousePos;
-
-      const mouseDiff = scalePoint(
-        diffPoints(currentMousePos, lastMousePos),
-        scale,
-      );
+  const updateOffset = useCallback(
+    (diff: Point): void => {
+      const scaledDiff = scalePoint(diff, scale);
 
       setOffset((prevOffset) => {
-        const newOffset = addPoints(prevOffset, mouseDiff);
+        const newOffset = addPoints(prevOffset, scaledDiff);
         return clampOffset(newOffset);
       });
     },
     [scale, clampOffset],
   );
 
+  const handleMouseMove = useCallback(
+    (event: MouseEvent): void => {
+      updateOffset({ x: event.movementX, y: event.movementY });
+    },
+    [updateOffset],
+  );
+
   /**
-   * Remove the mouse move listener when the mouse is released to stop panning.
+   * Remove the listeners when the mouse is released to stop panning.
    */
   const handleMouseUp = useCallback((): void => {
-    document.removeEventListener("mousemove", handleMouseMove);
-    document.removeEventListener("mouseup", handleMouseUp);
+    if (!containerRef.current) return;
+
+    containerRef.current.removeEventListener("mousemove", handleMouseMove);
+    containerRef.current.removeEventListener("mouseup", handleMouseUp);
+    containerRef.current.removeEventListener("mouseleave", handleMouseUp);
   }, [handleMouseMove]);
 
   /**
    * Only add the mouse move listener when you click down so that moving your mouse normally doesn't
    * cause the canvas to pan.
    */
-  const handleStartPan = useCallback(
-    (event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
-      document.addEventListener("mousemove", handleMouseMove);
-      document.addEventListener("mouseup", handleMouseUp);
-      lastMousePosRef.current = { x: event.pageX, y: event.pageY };
+  const handleStartMousePan = useCallback((): void => {
+    if (!containerRef.current) return;
+
+    containerRef.current.addEventListener("mousemove", handleMouseMove);
+    containerRef.current.addEventListener("mouseup", handleMouseUp);
+    containerRef.current.addEventListener("mouseleave", handleMouseUp);
+  }, [handleMouseMove, handleMouseUp]);
+
+  const handleTouchMove = useCallback(
+    (event: TouchEvent): void => {
+      const touchCount = event.touches.length;
+      event.preventDefault();
+
+      // TODO: Implement multi-touch zooming
+      if (touchCount !== 1) return;
+
+      // Check that the touch event is the same as the one that started the pan
+      if (event.touches[0].identifier !== startTouchesRef.current[0].identifier)
+        return;
+
+      const startTouch = startTouchesRef.current[0];
+      const touch = event.touches[0];
+
+      const touchDiff: Point = {
+        x: touch.pageX - startTouch.pageX,
+        y: touch.pageY - startTouch.pageY,
+      };
+
+      updateOffset({ x: touchDiff.x, y: touchDiff.y });
+      startTouchesRef.current = [touch];
     },
-    [handleMouseMove, handleMouseUp],
+    [updateOffset],
+  );
+
+  const handleTouchEnd = useCallback((): void => {
+    if (!canvasRef.current) return;
+
+    canvasRef.current.removeEventListener("touchmove", handleTouchMove);
+    canvasRef.current.removeEventListener("touchend", handleTouchEnd);
+    canvasRef.current.removeEventListener("touchcancel", handleTouchEnd);
+  }, [handleTouchMove]);
+
+  /**
+   * Note: The `React` prefix to `TouchEvent` is necessary to distinguish it from the non-react
+   * version used by handleTouchMove.
+   */
+  const handleStartTouchPan = useCallback(
+    (event: React.TouchEvent<HTMLDivElement>): void => {
+      if (!canvasRef.current) return;
+
+      const touchCount = event.touches.length;
+
+      // TODO: Implement multi-touch zooming
+      if (touchCount !== 1) return;
+
+      canvasRef.current.addEventListener("touchmove", handleTouchMove, {
+        passive: false,
+      });
+      canvasRef.current.addEventListener("touchend", handleTouchEnd);
+      canvasRef.current.addEventListener("touchcancel", handleTouchEnd);
+      startTouchesRef.current = Array.from(event.touches);
+    },
+    [handleTouchMove, handleTouchEnd],
   );
 
   return (
     <>
       <CanvasPicker />
-      <CanvasContainer onMouseDown={handleStartPan}>
-        {isLoading && <CircularProgress />}
+      <CanvasContainer
+        ref={containerRef}
+        onMouseDown={handleStartMousePan}
+        onTouchStart={handleStartTouchPan}
+      >
+        {isLoading && <CircularProgress className="loader" />}
         <canvas
           ref={canvasRef}
           id="canvas-pan-and-zoom"
