000770b892a48613fa98e0b1b27d8de0a9f6c690
Samuel Ou <sou323@aucklanduni.ac.nz>
2024-05-06 03:08:13 +1200

    feat: sorted colors by hue

--- packages/frontend/src/hooks/queries/usePalette.tsx
+++ packages/frontend/src/hooks/queries/usePalette.tsx
@@ -2,16 +2,22 @@
 
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import convert from "color-convert";
 
 import config from "@/config";
-import { BlurpleEvent, PaletteRequest } from "@blurple-canvas-web/types";
+import {
+  BlurpleEvent,
+  Palette,
+  PaletteRequest,
+} from "@blurple-canvas-web/types";
+import { RGB } from "color-convert/conversions";
 
 export function usePalette(eventId?: BlurpleEvent["id"]) {
   const getPalette = async () => {
     const response = await axios.get<PaletteRequest.ResBody>(
       `${config.apiUrl}/api/v1/palette/${eventId ?? "current"}`,
     );
-    return response.data;
+    return sortPalette(response.data);
   };
 
   return useQuery({
@@ -20,5 +26,13 @@ export function usePalette(eventId?: BlurpleEvent["id"]) {
     refetchOnMount: false,
     refetchOnWindowFocus: false,
     placeholderData: [] as PaletteRequest.ResBody,
+  });
+}
+
+function sortPalette(palette: Palette) {
+  return [...palette].sort((a, b) => {
+    const aHue = convert.rgb.hsl(a.rgba.slice(0, 3) as RGB)[0];
+    const bHue = convert.rgb.hsl(b.rgba.slice(0, 3) as RGB)[0];
+    return aHue - bHue;
   });
 }
