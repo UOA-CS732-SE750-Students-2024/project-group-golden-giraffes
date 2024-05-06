53fa3b8bec221affeec64b5dd9397a42c02edd3f
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-05-06 23:26:22 +1200

    feat: sort by OKLCH hue (#102)
    * feat: sorted colors by hue
    
    * refactor: swapped in color.js
    
    * fix: remove color-convert from pnpm-lock
    
    * use OKLCH
    
    ---------
    
    Co-authored-by: Samuel Ou <sou323@aucklanduni.ac.nz>

--- packages/frontend/src/hooks/queries/usePalette.tsx
+++ packages/frontend/src/hooks/queries/usePalette.tsx
@@ -5,13 +5,21 @@ import axios from "axios";
 
 import config from "@/config";
 import { BlurpleEvent, PaletteRequest } from "@blurple-canvas-web/types";
+import Color, { Coords } from "colorjs.io";
+import { OKLCH } from "colorjs.io/fn";
 
 export function usePalette(eventId?: BlurpleEvent["id"]) {
   const getPalette = async () => {
     const response = await axios.get<PaletteRequest.ResBody>(
       `${config.apiUrl}/api/v1/palette/${eventId ?? "current"}`,
     );
-    return response.data;
+    return response.data.sort((a, b) => {
+      const rgbA = a.rgba.slice(0, 3) as Coords;
+      const rgbB = b.rgba.slice(0, 3) as Coords;
+      const hueA = new Color("srgb", rgbA).to("oklch").coords[2];
+      const hueB = new Color("srgb", rgbB).to("oklch").coords[2];
+      return hueA - hueB;
+    });
   };
 
   return useQuery({
