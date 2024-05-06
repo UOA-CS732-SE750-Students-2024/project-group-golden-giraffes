fdf08abb2670de5e4945312abadcd978347c92f2
PolarWolf314 <aaronjoshguo@gmail.com>
2024-05-06 23:31:16 +1200

    test: add seed discord profiles

---
+++ packages/backend/src/test/seedDiscordProfile.ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/client";
+
+export default function seedDiscordProfiles() {
+  try {
+    prisma.discord_user_profile.createMany({
+      data: [
+        {
+          user_id: BigInt(204778476102877187n),
+          username: "rocked03",
+          profile_picture_url:
+            "https://cdn.discordapp.com/avatars/204778476102877187/f4468ea05fa0dada4e3a3fbe18b748fe.png",
+        },
+        {
+          user_id: BigInt(201892070091128832n),
+          username: "polarwolf314",
+          profile_picture_url:
+            "https://cdn.discordapp.com/avatars/201892070091128832/ef960949b260ce193b249710bb8c7a79.png",
+        },
+      ],
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
