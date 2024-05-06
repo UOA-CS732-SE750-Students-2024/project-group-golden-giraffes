3ddfc01b3dfe64d2c1977fccdace6b59809ce988
PolarWolf314 <aaronjoshguo@gmail.com>
2024-05-07 01:22:49 +1200

    refactor: remove rendundant `BigInt` call

--- packages/backend/src/test/seedDiscordProfile.ts
+++ packages/backend/src/test/seedDiscordProfile.ts
@@ -5,13 +5,13 @@ export default function seedDiscordProfiles() {
     prisma.discord_user_profile.createMany({
       data: [
         {
-          user_id: BigInt(204778476102877187n),
+          user_id: 204778476102877187n,
           username: "rocked03",
           profile_picture_url:
             "https://cdn.discordapp.com/avatars/204778476102877187/f4468ea05fa0dada4e3a3fbe18b748fe.png",
         },
         {
-          user_id: BigInt(201892070091128832n),
+          user_id: 201892070091128832n,
           username: "polarwolf314",
           profile_picture_url:
             "https://cdn.discordapp.com/avatars/201892070091128832/ef960949b260ce193b249710bb8c7a79.png",
