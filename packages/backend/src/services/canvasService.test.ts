83c6f3e365c0aa9156de2ca061e5fb6e9de5b0e2
boxy8 <zou.emily@yahoo.co.nz>
2024-05-07 17:47:09 +1200

    test: canvas info and pixels methods

---
+++ packages/backend/src/services/canvasService.test.ts
@@ -0,0 +1,82 @@
+import { prisma } from "@/client";
+import { BadRequestError, ForbiddenError, NotFoundError } from "@/errors";
+import {
+  seedBlacklist,
+  seedCanvases,
+  seedColors,
+  seedPixels,
+  seedUsers,
+} from "@/test";
+import { getCanvasInfo, getCanvasPixels, getCanvases } from "./canvasService";
+
+describe("Canvas Info Tests", () => {
+  beforeEach(() => {
+    seedCanvases();
+  });
+
+  it("Gets canvases", async () => {
+    expect((await getCanvases()).length).toBe(2);
+  });
+
+  it("Gets summary of canvases in desc order", async () => {
+    const canvases = await getCanvases();
+    expect(canvases).toMatchObject([
+      { id: 9, name: "Locked Canvas" },
+      { id: 1, name: "Unlocked Canvas" },
+    ]);
+  });
+
+  it("Gets canvas by ID", async () => {
+    expect(await getCanvasInfo(1)).toMatchObject({
+      id: 1,
+      name: "Unlocked Canvas",
+      width: 2,
+      height: 2,
+      isLocked: false,
+      eventId: 1,
+      startCoordinates: [1, 1],
+    });
+
+    expect(await getCanvasInfo(9)).toMatchObject({
+      id: 9,
+      name: "Locked Canvas",
+      width: 2,
+      height: 2,
+      isLocked: true,
+      eventId: 9,
+      startCoordinates: [1, 1],
+    });
+  });
+});
+
+describe("Canvas Validation Tests", () => {
+  beforeEach(() => {
+    seedCanvases();
+  });
+
+  it("Resolves valid canvas", async () => {
+    return expect(getCanvasInfo(1)).resolves.not.toThrow();
+  });
+
+  it("Rejects nonexistent canvas", async () => {
+    return expect(getCanvasInfo(9999)).rejects.toThrow(NotFoundError);
+  });
+});
+
+describe("Canvas Pixels Tests", () => {
+  beforeEach(() => {
+    seedCanvases();
+    seedColors();
+    seedPixels();
+  });
+
+  it("Gets canvas pixels", async () => {
+    const pixels = await getCanvasPixels(1);
+    expect(pixels.length).toBe(3);
+    expect(pixels[0]).toStrictEqual([88, 101, 242, 127]);
+    expect(pixels[1]).toStrictEqual([88, 101, 242, 255]);
+    expect(pixels[2]).toStrictEqual([234, 35, 40, 255]);
+  });
+});
+
+// TO DO - PNG Canvas Tests
