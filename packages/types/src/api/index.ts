88d4ff2701155d6b1f239573008a700ed301c8fe
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-05-07 22:39:17 +1200

    alphasort

--- packages/types/src/api/index.ts
+++ packages/types/src/api/index.ts
@@ -1,5 +1,5 @@
 export * as CanvasInfoRequest from "./CanvasInfoRequest";
 export * as CanvasListRequest from "./CanvasListRequest";
 export * as EventRequest from "./EventRequest";
-export * as PaletteRequest from "./PaletteRequest";
 export * as HistoryRequest from "./HistoryRequest";
+export * as PaletteRequest from "./PaletteRequest";
