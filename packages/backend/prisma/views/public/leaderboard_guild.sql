6f58b5cab93e5c80c9138aaf75b5ee66b5c64253
boxy8 <zou.emily@yahoo.co.nz>
2024-05-07 23:09:51 +1200

    feat: add discord user profile relation to leaderboard views

--- packages/backend/prisma/views/public/leaderboard_guild.sql
+++ packages/backend/prisma/views/public/leaderboard_guild.sql
@@ -2,6 +2,7 @@ SELECT
   history.user_id,
   history.canvas_id,
   history.guild_id,
+  history.discord_user_profile,
   count(*) AS total_pixels,
   rank() OVER (
     PARTITION BY history.canvas_id,
