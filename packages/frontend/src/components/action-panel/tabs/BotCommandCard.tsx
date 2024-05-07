580664ed46edcab543dc552d41a46a414d288399
Samuel Ou <sou323@aucklanduni.ac.nz>
2024-05-08 06:14:29 +1200

    feat: copy button

--- packages/frontend/src/components/action-panel/tabs/BotCommandCard.tsx
+++ packages/frontend/src/components/action-panel/tabs/BotCommandCard.tsx
@@ -1,18 +1,56 @@
 import { PaletteColor, Point } from "@blurple-canvas-web/types";
 import { styled } from "@mui/material";
+import { ClipboardCopy } from "lucide-react";
 
 const Wrapper = styled("div")`
-  align-items: baseline;
+  align-items: center;
   color: var(--discord-white-oklch);
-  display: grid;
+  display: flex;
   font-size: 1.2rem;
   grid-template-columns: 1fr auto;
 `;
 
 const Code = styled("code")`
   color: var(--discord-white-oklch);
+  flex: 1;
   letter-spacing: 0.05em;
   line-height: 1.1;
+`;
+
+interface CopyButtonProps {
+  backgroundColor: PaletteColor;
+  onClick?: () => void;
+}
+
+const CopyButton = styled("button", {
+  shouldForwardProp: (prop) => prop !== "backgroundColor",
+})<CopyButtonProps>`
+  background-color: oklch(var(--discord-white-oklch) / 12%);
+  border-radius: var(--card-border-radius);
+  border: none;
+  color: var(--discord-white-oklch);
+  cursor: pointer;
+  font-size: 1rem;
+  padding: 0.3rem;
+  transition: background-color var(--transition-duration-slow) ease;
+
+  > svg {
+    height: 1.325rem;
+  }
+
+  &:hover {
+    background-color: rgba(
+      ${({ backgroundColor }) => backgroundColor.rgba.slice(0, 3).join(", ")},
+      36%
+    );
+  }
+
+  &:active {
+    background-color: rgba(
+      ${({ backgroundColor }) => backgroundColor.rgba.slice(0, 3).join(", ")},
+      6%
+    );
+  }
 `;
 
 export default function BotCommandCard({
@@ -26,11 +64,19 @@ export default function BotCommandCard({
 
   const { x, y } = coordinates;
 
+  const command = `/place x:${x} y:${y} color:${color.code}`;
+
   return (
     <Wrapper>
-      <Code>
-        /place x:{x} y:{y} color:{color.code}
-      </Code>
+      <Code>{command}</Code>
+      <CopyButton
+        backgroundColor={color}
+        onClick={() => {
+          navigator.clipboard.writeText(command);
+        }}
+      >
+        <ClipboardCopy />
+      </CopyButton>
     </Wrapper>
   );
 }
