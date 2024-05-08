a041d503b6501ddfd6a654f7ed8bb488547fad33
Samuel Ou <78394628+sou323@users.noreply.github.com>
2024-05-08 18:05:58 +1200

    feat: added bot command card (#154)
    * feat: added bot command card
    
    * refactor: coordinates var

---
+++ packages/frontend/src/components/action-panel/tabs/BotCommandCard.tsx
@@ -0,0 +1,74 @@
+import { PaletteColor, Point } from "@blurple-canvas-web/types";
+import { styled } from "@mui/material";
+import { Copy as CopyIcon } from "lucide-react";
+
+const Wrapper = styled("div")`
+  align-items: center;
+  color: var(--discord-white-oklch);
+  display: flex;
+  font-size: 0.9rem;
+  grid-template-columns: 1fr auto;
+`;
+
+const Code = styled("code")`
+  color: var(--discord-white-oklch);
+  flex: 1;
+  letter-spacing: 0.05em;
+  line-height: 1.1;
+`;
+
+interface CopyButtonProps {
+  onClick?: () => void;
+}
+
+const StyledCopyIcon = styled(CopyIcon)`
+  height: 1.325rem;
+`;
+
+const CopyButton = styled("button")<CopyButtonProps>`
+  background-color: oklch(var(--discord-white-oklch) / 12%);
+  border-radius: 0.5rem;
+  border: none;
+  color: var(--discord-white-oklch);
+  cursor: pointer;
+  display: flex;
+  font-size: 1rem;
+  padding: 0.4rem 0.3rem;
+  place-items: center;
+  transition: background-color var(--transition-duration-slow) ease;
+
+  &:hover {
+    background-color: oklch(var(--discord-white-oklch) / 24%);
+  }
+
+  &:active {
+    background-color: oklch(var(--discord-white-oklch) / 6%);
+  }
+`;
+
+export default function BotCommandCard({
+  color,
+  coordinates,
+}: {
+  color?: PaletteColor | null;
+  coordinates: Point;
+}) {
+  if (!color) return <Wrapper>No color selected</Wrapper>;
+
+  const { x, y } = coordinates;
+
+  const command = `/place x:${x} y:${y} color:${color.code}`;
+
+  return (
+    <Wrapper>
+      <Code>{command}</Code>
+      <CopyButton
+        onClick={() => {
+          navigator.clipboard.writeText(command);
+        }}
+      >
+        <StyledCopyIcon />
+      </CopyButton>
+    </Wrapper>
+  );
+}
