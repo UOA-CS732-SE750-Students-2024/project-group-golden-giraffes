588f1c0541736c74457eb6fa6b57c365be4f47c0
PolarWolf314 <aaronjoshguo@gmail.com>
2024-05-08 21:40:42 +1200

    style: update formatting

--- packages/frontend/src/components/action-panel/tabs/PlacePixelTab.tsx
+++ packages/frontend/src/components/action-panel/tabs/PlacePixelTab.tsx
@@ -87,7 +87,9 @@ export default function PlacePixelTab({
           "Place pixel"
         : "Select a pixel"}
         <CoordinateLabel>
-          {selectedCoordinates && selectedColor ? `(${x},\u00A0${y})` /* Nonbreaking space */ : undefined}
+          {selectedCoordinates && selectedColor ?
+            `(${x},\u00A0${y})` /* Nonbreaking space */
+          : undefined}
         </CoordinateLabel>
       </DynamicButton>
       {!selectedColor?.global && serverInvite && (
