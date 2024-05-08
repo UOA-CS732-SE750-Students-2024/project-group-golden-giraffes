3a0f1fa88bc8b6793c796bdd1100a9223155ac6d
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-05-08 12:35:36 +1200

    feat: add test page and endpoint for auth

---
+++ packages/frontend/src/app/testauth/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import config from "@/config";
+import { useEffect, useState } from "react";
+
+export default function testauth() {
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const [data, setData] = useState<any | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${config.apiUrl}/api/v1/discord/test`,
+        /* { credentials: "same-origin", } */
+      );
+      const data = await response.json();
+      setData(data);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading…</div>;
+  }
+
+  console.log(data);
+  return <div>Finished Loading</div>;
+}
