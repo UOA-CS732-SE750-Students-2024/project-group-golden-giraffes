25fdc7663619d0a3c9ba74062880ad250361fe5b
boxy8 <zou.emily@yahoo.co.nz>
2024-05-08 17:10:28 +1200

    refactor: order imports alphabetically

--- packages/frontend/src/contexts/index.ts
+++ packages/frontend/src/contexts/index.ts
@@ -1,10 +1,10 @@
+export {
+  ActiveCanvasProvider,
+  useActiveCanvasContext,
+} from "./ActiveCanvasContext";
 export { AuthProvider, useAuthContext } from "./AuthProvider";
 export { QueryClientProvider } from "./QueryClientProvider";
 export {
   SelectedColorProvider,
   useSelectedColorContext,
 } from "./SelectedColorContext";
-export {
-  ActiveCanvasProvider,
-  useActiveCanvasContext,
-} from "./ActiveCanvasContext";
