fe468ee36d0dd23ba4090650cb5777b38d5dd242
pumbas600 <joshjeffers600@gmail.com>
2024-05-09 15:47:21 +1200

    feat: broadcast placed pixels

--- packages/types/src/api/PlacePixelSocketPayload.d.ts
+++ packages/types/src/api/PlacePixelSocketPayload.d.ts
@@ -1,7 +1,7 @@
 import { PixelColor } from "../palette";
-import { Point } from "../point";
 
 export interface Payload {
-  coords: Point;
-  color: PixelColor;
+  x: number;
+  y: number;
+  rgba: PixelColor;
 }
