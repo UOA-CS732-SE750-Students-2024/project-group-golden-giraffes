907c6a6d2e7341c7770642b5b7f30af0efe7bd4e
Samuel Ou <78394628+sou323@users.noreply.github.com>
2024-05-10 15:28:57 +1200

    feat: added fancy reticle (#221)
    * feat: added fancy reticle
    
    * feat: readded crosshair when zoomed out
    
    * refactor: tidy up

--- packages/frontend/src/components/canvas/generatePreviewPixel.ts
+++ packages/frontend/src/components/canvas/generatePreviewPixel.ts
@@ -11,6 +11,7 @@ export default function updateCanvasPreviewPixel(
   canvasRef: RefObject<HTMLCanvasElement>,
   pixelPoint: Point,
   color: PaletteColor | null,
+  alpha = 0,
 ) {
   const context = canvasRef.current?.getContext("2d");
   if (!context) {
@@ -25,11 +26,11 @@ export default function updateCanvasPreviewPixel(
   context.clearRect(0, 0, width, height);
 
   // draw a 5x5 white square around the pixel
-  context.fillStyle = "white";
+  context.fillStyle = `rgba(255, 255, 255, ${alpha})`;
   context.fillRect(x - 6, y - 6, 13, 13);
 
   // draw a 3x3 black square around the pixel
-  context.fillStyle = "black";
+  context.fillStyle = `rgba(0, 0, 0, ${alpha})`;
   context.fillRect(x - 3, y - 3, 7, 7);
 
   // clear quadrants to make a cross
