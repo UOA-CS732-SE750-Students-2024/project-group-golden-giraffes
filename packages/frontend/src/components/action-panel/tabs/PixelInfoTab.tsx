072ef1de6c46327601df9509360706461a95a3a2
Samuel Ou <78394628+sou323@users.noreply.github.com>
2024-05-11 15:54:11 +1200

    feat: history skeleton (#224)
    * feat: sticky headers and footers in action panel
    
    * fix: resorted overflow in css
    
    * feat: history loading skeleton
    
    * fix: removed weird duplicate
    
    * refactor: better skeleton
    
    * refactor: removed now-unused classes
    
    * refactor: changed the length again
    
    * Update packages/frontend/src/components/action-panel/tabs/PixelHistoryListItem.tsx
    
    Co-authored-by: Josh Jeffers <38820160+pumbas600@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Josh Jeffers <38820160+pumbas600@users.noreply.github.com>

--- packages/frontend/src/components/action-panel/tabs/PixelInfoTab.tsx
+++ packages/frontend/src/components/action-panel/tabs/PixelInfoTab.tsx
@@ -26,15 +26,9 @@ interface PixelHistoryProps {
 }
 
 const PixelHistoryPast = ({ isLoading, history }: PixelHistoryProps) => {
-  // TODO: Replace with skeleton
-  if (isLoading) {
+  if (isLoading && history.length === 0) {
     return;
   }
-
-  if (history.length === 0) {
-    return;
-  }
-
   const pastPixelHistory = history.slice(1); // [] if out of index
 
   return (
@@ -54,9 +48,8 @@ const PixelHistoryPast = ({ isLoading, history }: PixelHistoryProps) => {
 };
 
 const PixelHistoryCurrent = ({ isLoading, history }: PixelHistoryProps) => {
-  // TODO: Replace with skeleton
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <PixelHistoryListItem />;
   }
 
   if (history.length === 0) {
