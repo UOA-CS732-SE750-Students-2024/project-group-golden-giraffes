8482d66107588696d83c1328b496dbc127d1ce64
Henry  Wang <wang.henryhan@gmail.com>
2025-03-22 21:52:25 +1300

    setup pointer events

--- packages/frontend/src/components/slideable-drawer/SlideableDrawer.tsx
+++ packages/frontend/src/components/slideable-drawer/SlideableDrawer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { styled } from "@mui/material";
+import { useCallback } from "react";
 
 const Wrapper = styled("div")`
   ${({ theme }) => theme.breakpoints.down("md")} {
@@ -53,10 +54,51 @@ interface SlideableDrawerProps {
 }
 
 export default function SlideableDrawer({ children }: SlideableDrawerProps) {
+  /**
+   * Defaults to pan when a single pointer is down, and zoom when two pointers are down.
+   */
+  const handlePointerMove = useCallback((event: PointerEvent): void => {
+    const elem = event.currentTarget;
+    // Only handle primary pointers to prevent duplicate handling
+    if (!(elem instanceof HTMLElement)) return;
+    console.log(event.movementX);
+  }, []);
+
+  /**
+   * Remove the listeners when the mouse is released to stop panning.
+   */
+  const handlePointerUp = useCallback(
+    (event: PointerEvent): void => {
+      const elem = event.currentTarget;
+      if (!(elem instanceof HTMLElement)) return;
+      elem.releasePointerCapture(event.pointerId);
+
+      elem.removeEventListener("pointermove", handlePointerMove);
+      elem.removeEventListener("pointerup", handlePointerUp);
+      elem.removeEventListener("pointercancel", handlePointerUp);
+    },
+    [handlePointerMove],
+  );
+
+  /**
+   * Only add the mouse move listener when you click down so that moving your mouse normally doesn't
+   * cause the canvas to pan.
+   */
+  const handlePointerDown = useCallback(
+    (event: React.PointerEvent<HTMLDivElement>) => {
+      const elem = event.currentTarget;
+      elem.setPointerCapture(event.pointerId);
+      elem.addEventListener("pointermove", handlePointerMove);
+      elem.addEventListener("pointerup", handlePointerUp);
+      elem.addEventListener("pointercancel", handlePointerUp);
+    },
+    [handlePointerMove, handlePointerUp],
+  );
+
   return (
     <>
       {/* Doing it this way as handlers are directly applied to DrawerWrapper (though it could also be possible to dynamically disable them through code) */}
-      <DrawerWrapper>
+      <DrawerWrapper onPointerDown={handlePointerDown}>
         <HandleWrapper>
           <Handle />
         </HandleWrapper>
