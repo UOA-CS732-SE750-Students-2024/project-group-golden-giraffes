598bc4d113de57e7b0c752ab6eb04b786d949a42
Henry  Wang <wang.henryhan@gmail.com>
2025-03-23 22:16:49 +1300

    move discord invite link to top mobile view

--- packages/frontend/src/components/canvas/CanvasView.tsx
+++ packages/frontend/src/components/canvas/CanvasView.tsx
@@ -69,7 +69,6 @@ const PreviewPixel = styled("div")`
 const InviteButton = styled(Button)`
   background-color: oklch(var(--discord-legacy-dark-but-not-black-oklch) / 80%);
   border-radius: 0.5rem 0.5rem 1rem 0.5rem;
-  bottom: 0.5rem;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
   color: white;
   font-size: 1.2rem;
@@ -83,6 +82,14 @@ const InviteButton = styled(Button)`
 
   :hover {
     background-color: var(--discord-blurple);
+  }
+
+  ${({ theme }) => theme.breakpoints.up("md")} {
+    bottom: 0.5rem;
+  }
+
+  ${({ theme }) => theme.breakpoints.down("md")} {
+    top: 0.5rem;
   }
 `;
 
