a06dcee822352ac5554c83470556089ac0272f49
Henry <100452681+hwan513@users.noreply.github.com>
2025-05-07 22:15:16 +1200

    feat(frontend): zooming and panning improvements (#262)
    * apply Safari transform3D fix to `transform` instead of -webkit-transform
    
    * remove scroll animation code in favour of css transition
    
    * disable transition on panning and enable on zoom
    
    * disable animations in safari
    
    * use pointerdown instead of mousedown for handleCanvasClick
    
    * use pointer events to handle both touch and mouse panning
    
    * rename canvas event handlers
    
    * apply event handlers directly event.currentTarget
    
    * allow for panning to exceed bounds of container elements
    
    * restrict decay velocity to 1 dp precision
    
    * extract PAN_DECAY into constant
    
    * add comments
    
    * extract getRelativeMousePosition
    
    * extract getRelativeMousePosition
    
    * improve scrolling calculation to work with sub"pixels" and outside bound
    
    * update scroll calculation to work with high polling wheel events
    
    * use initial zoom in zoom clamping
    
    * fixes old zoom calculation logic
    
    * remove unused code
    
    * getRelativeMousePosition to function from callback
    
    * fix usecallback depenedency array
    
    * use "none" | "initial" for changeGlobalSelectStyle
    
    * use undefined instead of "" in transition check
    
    * moves changeGlobalSelectStyle into `styled` css from useCallback
    
    * move event.deltaY guard return into earlier statement
    
    * use isZooming state instead of transitionDuration
    
    * MIN_ZOOM -> MIN_ZOOM_FACTOR; also added comments for zoom vars

--- packages/frontend/src/components/canvas/CanvasView.tsx
+++ packages/frontend/src/components/canvas/CanvasView.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CircularProgress, css, styled } from "@mui/material";
-import { Touch, useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import { PlacePixelSocket, Point } from "@blurple-canvas-web/types";
 
@@ -28,7 +28,15 @@ const CanvasContainer = styled("div")`
   place-content: center;
   place-items: center;
   /* Fixes blurry canvas in Safari when canvasImage overlaps with overflow, don't ask why */
-  -webkit-transform: translate3d(0, 0, 0);
+  transform: translate3d(0, 0, 0);
+  /* Don't handle panning and zooming with browser */
+  touch-action: none;
+
+  /* A bit heavy handed, but it prevents elements outside of the canvas from being selected during panning */
+  body:has(&:active) {
+    --webkit-user-select: none;
+    user-select: none;
+  }
 
   ${({ theme }) => theme.breakpoints.up("md")} {
     grid-row: 1 / -1;
@@ -128,9 +136,29 @@ function getDefaultZoom(
   return scale;
 }
 
-const SCALE_FACTOR = 0.2;
+/**
+ * Calculate the position of the mouse relative to the given element
+ **/
+function getRelativeMousePosition(element: HTMLElement, event: MouseEvent) {
+  const rect = element.getBoundingClientRect();
+  return { x: event.clientX - rect.left, y: event.clientY - rect.top };
+}
+
+// Arbitrary value applied to the deltaY of the wheel zoom function to make it feel right
+const SCALE_FACTOR = 0.002;
+// MAX ZOOM is the absolute maximum scaling that can be applied to the image element
 const MAX_ZOOM = 100;
-const MIN_ZOOM = 0.5;
+// MIN ZOOM_FACTOR is relative to the initalZoom. i.e. MIN_ZOOM_FACTOR = 0.9 -> minimumCssScale = 0.9 * initialZoom
+const MIN_ZOOM_FACTOR = 0.9;
+
+const PAN_DECAY = 0.75;
+// Transition animation on canvas pan and zoom is blurred on Safari and needs to be disabled.
+// If the user spoof their user agent, this is not my problem.
+// Bug in question https://bugs.webkit.org/show_bug.cgi?id=27684
+const IS_SAFARI =
+  navigator.userAgent.includes("Safari/") &&
+  !navigator.userAgent.includes("Chrome/") &&
+  !navigator.userAgent.includes("Chromium/");
 
 // This is to avoid weird business with the reticle not sizing properly
 const RETICLE_ORIGINAL_SCALE = 10;
@@ -152,26 +180,26 @@ export default function CanvasView() {
   const containerRef = useRef<HTMLDivElement>(null);
   const canvasImageWrapperRef = useRef<HTMLImageElement>(null);
   const canvasPanAndZoomRef = useRef<HTMLDivElement>(null);
-  const startTouchesRef = useRef<Touch[]>([]);
 
   const { color } = useSelectedColorContext();
   const { canvas, coords, setCoords } = useCanvasContext();
 
   const [isLoading, setIsLoading] = useState(false);
   const [zoom, setZoom] = useState(1);
+  const [initialZoom, setInitialZoom] = useState(1);
   const [offset, setOffset] = useState(ORIGIN);
   const [velocity, setVelocity] = useState<Point>({ x: 0, y: 0 });
   const [controlledPan, setControlledPan] = useState(false);
-  const [targetZoom, setTargetZoom] = useState(1);
-  const [mouseOffsetDirection, setMouseOffsetDirection] = useState(ORIGIN);
+  // Only applies to when zooming is triggered by wheel event
+  const [isZooming, setIsZooming] = useState(false);
 
   const imageUrl = `${config.apiUrl}/api/v1/canvas/${canvas.id}`;
   const handleLoadImage = useCallback((image: HTMLImageElement): void => {
-    const initialZoom =
+    const zoom =
       containerRef.current ? getDefaultZoom(containerRef.current, image) : 1;
 
-    setZoom(initialZoom);
-    setTargetZoom(initialZoom);
+    setInitialZoom(zoom);
+    setZoom(zoom);
     setVelocity(ORIGIN);
     setOffset(ORIGIN);
     setIsLoading(false);
@@ -268,34 +296,47 @@ export default function CanvasView() {
      */
     const handleWheel = (event: WheelEvent): void => {
       event.preventDefault();
-
-      const mousePositionOnCanvas: Point = {
-        x: event.offsetX,
-        y: event.offsetY,
-      };
       // Ensures that the handler can be added to a parent element but only operates on the canvas image wrapper.
       // Applying the handler to lower elements for some isn't consistently picked up in certain browsers (Firefox and Chrome).
       // Ideally, the scrolling should work outside of canvas-image-wrapper, but I can't seem to get the behaviour correct.
-      const elem = event.target;
-      if (!(elem instanceof HTMLElement)) return;
-      if (elem.id !== "canvas-image-wrapper") return;
+      const elem = event.currentTarget;
+      if (!(elem instanceof HTMLElement) || event.deltaY === 0) return;
+      const mousePositionOnCanvas = getRelativeMousePosition(elem, event);
 
-      // The mouse position's origin is in the top left of the canvas. The offset's origin is the
-      // center of the canvas so we do this to convert between the two.
-      setMouseOffsetDirection(
-        diffPoints(
-          {
-            x: elem.offsetWidth / 2,
-            y: elem.offsetHeight / 2,
-          },
-          mousePositionOnCanvas,
-        ),
+      // The mouse position's origin is in the top left of the container.
+      // Converts this to the offset from the center of the visual container
+      const mouseOffset = diffPoints(
+        { x: elem.offsetWidth / 2, y: elem.offsetHeight / 2 },
+        mousePositionOnCanvas,
       );
 
-      const scale = Math.exp(Math.sign(-event.deltaY) * SCALE_FACTOR);
-      const newZoom = clamp(targetZoom * scale, MIN_ZOOM, MAX_ZOOM);
+      // Inclusion of deltaY in calculation to account for different polling rate devices
+      // Could try logarithmic scale for smoother increments
+      const scale = 1 + SCALE_FACTOR * Math.max(Math.abs(event.deltaY), 1);
+      const newZoom = event.deltaY > 0 ? zoom / scale : zoom * scale;
+      const clampedZoom = clamp(
+        newZoom,
+        MIN_ZOOM_FACTOR * initialZoom,
+        MAX_ZOOM,
+      );
 
-      setTargetZoom(newZoom);
+      // Clamping the zoom means the actual scale may be different.
+      const clampedScale = clampedZoom / zoom;
+
+      setOffset((prevOffset) => {
+        // Calculate the of the mouse position relative to the center of the canvas in the container
+        const trueOffset = addPoints(prevOffset, mouseOffset);
+
+        // The amount we shift is scaled based on the amount we've zoomed in.
+        // Adds an extra shift based on the new scale of the canvas and the true offset
+        // Goodbye old comment with old implementation
+        const scaledOffsetDiff = multiplyPoint(trueOffset, clampedScale - 1);
+        return clampOffset(addPoints(scaledOffsetDiff, prevOffset), newZoom);
+      });
+
+      // Use css transition for zoom due to macOS trackpads having high polling rates resulting in laggy zooming if implemented differently
+      setIsZooming(true);
+      setZoom(clampedZoom);
     };
 
     containerRef.current?.addEventListener("wheel", handleWheel, {
@@ -304,49 +345,7 @@ export default function CanvasView() {
 
     return () =>
       containerRef.current?.removeEventListener("wheel", handleWheel);
-  }, [targetZoom]);
-
-  useEffect(() => {
-    if (zoom === targetZoom) return;
-
-    const glideZoom = () => {
-      const diff = (targetZoom - zoom) / targetZoom;
-      const scale = Math.exp(
-        Math.sign(diff) * SCALE_FACTOR * Math.abs(diff) * 2,
-      );
-      const newZoom = clamp(
-        zoom * scale,
-        diff > 0 ? MIN_ZOOM : targetZoom,
-        diff < 0 ? MAX_ZOOM : targetZoom,
-      );
-
-      const effectiveScale = newZoom / zoom;
-
-      setOffset((prevOffset) => {
-        // The direction we need to shift the offset to keep the pixel in the same place
-        const offsetDif = diffPoints(mouseOffsetDirection, prevOffset);
-
-        // The amount we shift is scaled based on the amount we've zoomed in.
-        const scaledOffsetDiff = multiplyPoint(
-          offsetDif,
-          // If the scale is 1, we've not zoomed in at all and so this multiplier becomes 0
-          // (causing no offset). If the scale is greater than 1, we're zooming in. A larger scale
-          // corresponds to a larger step (as 1/effectiveScale approaches 0). If the scale is less
-          // than 1, we're zooming out. In this case, 1 / effective scale becomes greater than 1,
-          // causing a negative offset. Thanks Henry for figuring out this equation 🙏.
-          1 - 1 / effectiveScale,
-        );
-
-        return clampOffset(addPoints(scaledOffsetDiff, prevOffset));
-      });
-
-      setZoom(newZoom);
-    };
-
-    const interval = setInterval(glideZoom, 8);
-
-    return () => clearInterval(interval);
-  }, [zoom, targetZoom, mouseOffsetDirection]);
+  }, [initialZoom, zoom]);
 
   /********************************
    * PANNING FUNCTIONALITY.       *
@@ -357,13 +356,13 @@ export default function CanvasView() {
    * screen along an axis.
    */
   const clampOffset = useCallback(
-    (offset: Point): Point => {
+    (offset: Point, zoom: number): Point => {
       const widthLimit = canvas.width / 2;
       const heightLimit = canvas.height / 2;
 
       return {
-        x: clamp(offset.x, -widthLimit, widthLimit),
-        y: clamp(offset.y, -heightLimit, heightLimit),
+        x: clamp(offset.x, -widthLimit * zoom, widthLimit * zoom),
+        y: clamp(offset.y, -heightLimit * zoom, heightLimit * zoom),
       };
     },
     [canvas],
@@ -376,116 +375,69 @@ export default function CanvasView() {
 
       setOffset((prevOffset) => {
         const newOffset = addPoints(prevOffset, scaledDiff);
-        return clampOffset(newOffset);
+        return clampOffset(newOffset, zoom);
       });
     },
     [zoom, clampOffset],
   );
 
-  const handleMouseMove = useCallback(
-    (event: MouseEvent): void => {
-      const diff = { x: event.movementX, y: event.movementY };
-      setVelocity({ x: diff.x, y: diff.y });
-      updateOffset(diff);
+  const handlePan = useCallback(
+    (event: PointerEvent): void => {
+      // Disable transitions while panning
+      setIsZooming(false);
+
+      const offset = { x: event.movementX, y: event.movementY };
+      const scaledOffset = multiplyPoint(offset, zoom);
+      setVelocity({ x: scaledOffset.x, y: scaledOffset.y });
+      updateOffset(scaledOffset);
     },
-    [updateOffset],
+    [updateOffset, zoom],
   );
 
   /**
    * Remove the listeners when the mouse is released to stop panning.
    */
-  const handleMouseUp = useCallback((): void => {
-    if (!containerRef.current) return;
+  const handlePointerUp = useCallback(
+    (event: PointerEvent): void => {
+      const elem = event.currentTarget;
+      if (!(elem instanceof HTMLElement)) return;
+      elem.releasePointerCapture(event.pointerId);
 
-    setControlledPan(false);
+      setControlledPan(false);
 
-    containerRef.current.removeEventListener("mousemove", handleMouseMove);
-    containerRef.current.removeEventListener("mouseup", handleMouseUp);
-    containerRef.current.removeEventListener("mouseleave", handleMouseUp);
-  }, [handleMouseMove]);
+      elem.removeEventListener("pointermove", handlePan);
+      elem.removeEventListener("pointerup", handlePointerUp);
+      elem.removeEventListener("pointercancel", handlePointerUp);
+    },
+    [handlePan],
+  );
 
   /**
    * Only add the mouse move listener when you click down so that moving your mouse normally doesn't
    * cause the canvas to pan.
    */
-  const handleStartMousePan = useCallback((): void => {
-    if (!containerRef.current) return;
+  const handlePointerDown = useCallback(
+    (event: React.PointerEvent<HTMLDivElement>) => {
+      const elem = event.currentTarget;
+      elem.setPointerCapture(event.pointerId);
+      setControlledPan(true);
 
-    setControlledPan(true);
-
-    containerRef.current.addEventListener("mousemove", handleMouseMove);
-    containerRef.current.addEventListener("mouseup", handleMouseUp);
-    containerRef.current.addEventListener("mouseleave", handleMouseUp);
-  }, [handleMouseMove, handleMouseUp]);
-
-  const handleTouchMove = useCallback(
-    (event: TouchEvent): void => {
-      const touchCount = event.touches.length;
-      event.preventDefault();
-
-      // TODO: Implement multi-touch zooming
-      if (touchCount !== 1) return;
-
-      // Check that the touch event is the same as the one that started the pan
-      if (event.touches[0].identifier !== startTouchesRef.current[0].identifier)
-        return;
-
-      const startTouch = startTouchesRef.current[0];
-      const touch = event.touches[0];
-
-      const touchDiff: Point = {
-        x: touch.pageX - startTouch.pageX,
-        y: touch.pageY - startTouch.pageY,
-      };
-
-      setVelocity(touchDiff);
-
-      updateOffset({ x: touchDiff.x, y: touchDiff.y });
-      startTouchesRef.current = [touch];
+      elem.addEventListener("pointermove", handlePan);
+      elem.addEventListener("pointerup", handlePointerUp);
+      elem.addEventListener("pointercancel", handlePointerUp);
     },
-    [updateOffset],
+    [handlePan, handlePointerUp],
   );
 
-  const handleTouchEnd = useCallback((): void => {
-    if (!containerRef.current) return;
-
-    containerRef.current.removeEventListener("touchmove", handleTouchMove);
-    containerRef.current.removeEventListener("touchend", handleTouchEnd);
-    containerRef.current.removeEventListener("touchcancel", handleTouchEnd);
-  }, [handleTouchMove]);
-
-  /**
-   * Note: The `React` prefix to `TouchEvent` is necessary to distinguish it from the non-react
-   * version used by handleTouchMove.
-   */
-  const handleStartTouchPan = useCallback(
-    (event: React.TouchEvent<HTMLDivElement>): void => {
-      if (!containerRef.current) return;
-
-      const touchCount = event.touches.length;
-
-      // TODO: Implement multi-touch zooming
-      if (touchCount !== 1) return;
-
-      containerRef.current.addEventListener("touchmove", handleTouchMove, {
-        passive: false,
-      });
-      containerRef.current.addEventListener("touchend", handleTouchEnd);
-      containerRef.current.addEventListener("touchcancel", handleTouchEnd);
-      startTouchesRef.current = Array.from(event.touches);
-    },
-    [handleTouchMove, handleTouchEnd],
-  );
-
+  // Could potentially get replaced by a transition animation with ease, however this will work on Safari
   useEffect(() => {
     const decayVelocity = () => {
-      if (velocity.x === 0 && velocity.y === 0) return;
+      if (velocity.x < 0.1 && velocity.y < 0.1) return;
       if (controlledPan) return;
       updateOffset(velocity);
-      const decay = 0.75;
       setVelocity((prevVelocity) => ({
-        x: prevVelocity.x * decay,
-        y: prevVelocity.y * decay,
+        x: prevVelocity.x * PAN_DECAY,
+        y: prevVelocity.y * PAN_DECAY,
       }));
     };
 
@@ -504,37 +456,33 @@ export default function CanvasView() {
    * When the canvas is clicked, we want to know which pixel was clicked on.
    */
   const handleCanvasClick = useCallback(
-    (event: MouseEvent): void => {
-      if (!(event.target instanceof HTMLElement)) return;
-      const canvas = event.target;
-      const canvasRect = canvas.getBoundingClientRect();
+    (event: PointerEvent): void => {
+      if (!(event.currentTarget instanceof HTMLElement) || !event.isPrimary)
+        return;
+      const canvas = event.currentTarget;
+      // Use boundingClientRect for more accurate pixel positioning
+      const relativeMousePos = getRelativeMousePosition(canvas, event);
+      const canvasPos = dividePoint(relativeMousePos, zoom);
 
-      const mouseX = event.clientX - canvasRect.left;
-      const mouseY = event.clientY - canvasRect.top;
-
-      const imageX = mouseX / zoom;
-      const imageY = mouseY / zoom;
-
-      const boundedX = clamp(Math.floor(imageX), 0, canvas.offsetWidth - 1);
-      const boundedY = clamp(Math.floor(imageY), 0, canvas.offsetHeight - 1);
+      const boundedCanvasPos = {
+        x: clamp(Math.floor(canvasPos.x), 0, canvas.offsetWidth - 1),
+        y: clamp(Math.floor(canvasPos.y), 0, canvas.offsetHeight - 1),
+      };
       // we only care about updating the location
-      setCoords({
-        x: boundedX,
-        y: boundedY,
-      });
+      setCoords(boundedCanvasPos);
     },
     [zoom, setCoords],
   );
 
   useEffect(() => {
     canvasImageWrapperRef.current?.addEventListener(
-      "mousedown",
+      "pointerdown",
       handleCanvasClick,
     );
 
     return () =>
       canvasImageWrapperRef.current?.removeEventListener(
-        "mousedown",
+        "pointerdown",
         handleCanvasClick,
       );
   }, [handleCanvasClick]);
@@ -543,11 +491,7 @@ export default function CanvasView() {
 
   return (
     <>
-      <CanvasContainer
-        ref={containerRef}
-        onMouseDown={handleStartMousePan}
-        onTouchStart={handleStartTouchPan}
-      >
+      <CanvasContainer ref={containerRef} onPointerDown={handlePointerDown}>
         {config.discordServerInvite && (
           <a href={config.discordServerInvite} target="_blank" rel="noreferrer">
             <InviteButton>Project Blurple</InviteButton>
@@ -557,8 +501,12 @@ export default function CanvasView() {
           id="canvas-pan-and-zoom"
           ref={canvasPanAndZoomRef}
           style={{
-            transform: `translate(${offset.x}px, ${offset.y}px)`,
-            scale: zoom,
+            transform: `matrix(${zoom}, 0, 0, ${zoom}, ${offset.x}, ${offset.y})`,
+            // Only apply transition when zooming is triggered by wheel event
+            transition:
+              !IS_SAFARI && isZooming ?
+                "transform var(--transition-duration-fast) ease-out"
+              : undefined,
           }}
         >
           <ReticleContainer
