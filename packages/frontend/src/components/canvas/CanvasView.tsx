3f8fee2814864ebf10365a37a71564563fac6d7c
Henry <100452681+hwan513@users.noreply.github.com>
2025-05-07 22:27:15 +1200

    fix(frontend): add isLaunching to address initial canvas zoom incorrectness (#263)
    * add an isLaunching state to prevent initial canvas size issue
    
    * use should forward props for CanvasImage wrapper styled
    
    Co-authored-by: Jasper Lai <33956381+jaskfla@users.noreply.github.com>
    
    * style: fix formatting
    
    * fix: add type to prop and use correct function
    
    ---------
    
    Co-authored-by: Jasper Lai <33956381+jaskfla@users.noreply.github.com>

--- packages/frontend/src/components/canvas/CanvasView.tsx
+++ packages/frontend/src/components/canvas/CanvasView.tsx
@@ -92,7 +92,13 @@ const InviteButton = styled(Button)`
   }
 `;
 
-const CanvasImageWrapper = styled("div")<{ isLoading: boolean }>`
+const CanvasImageWrapper = styled("div", {
+  shouldForwardProp: (prop: string) =>
+    !["isLaunching", "isLoading"].includes(prop),
+})<{
+  isLoading: boolean;
+  isLaunching: boolean;
+}>`
   transition: filter var(--transition-duration-medium) ease;
   ${({ isLoading }) =>
     isLoading &&
@@ -104,6 +110,11 @@ const CanvasImageWrapper = styled("div")<{ isLoading: boolean }>`
   position: relative;
 
   img {
+    ${({ isLaunching }) =>
+      isLaunching &&
+      css`
+        visibility: hidden;
+      `}
     image-rendering: pixelated;
     pointer-events: none;
   }
@@ -184,7 +195,8 @@ export default function CanvasView() {
   const { color } = useSelectedColorContext();
   const { canvas, coords, setCoords } = useCanvasContext();
 
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLaunching, setIsLaunching] = useState(true);
   const [zoom, setZoom] = useState(1);
   const [initialZoom, setInitialZoom] = useState(1);
   const [offset, setOffset] = useState(ORIGIN);
@@ -203,6 +215,7 @@ export default function CanvasView() {
     setVelocity(ORIGIN);
     setOffset(ORIGIN);
     setIsLoading(false);
+    setIsLaunching(false);
   }, []);
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: We want to show the loader when switching canvases
@@ -544,6 +557,7 @@ export default function CanvasView() {
           <CanvasImageWrapper
             ref={canvasImageWrapperRef}
             isLoading={isLoading}
+            isLaunching={isLaunching}
             id="canvas-image-wrapper"
           >
             <img
